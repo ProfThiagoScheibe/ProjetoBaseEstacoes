52744d644ef45c0a8752eba65217f035ef9641a9
ProfThiagoScheibe <134386393+ProfThiagoScheibe@users.noreply.github.com>
2023-10-30 21:40:48 -0300

    alrte

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const aplausos = document.querySelector("#som_tecla_aplausos")
 function tocaSom(som) {
     alert ("clap, clap, clap") 
 }
+
+let controle = "tocar"
+if(comando == "tocar"){
+    alert("Áudio pronto para tocar");
+} else{
+    alert("O áudio não está pronto")
+}
